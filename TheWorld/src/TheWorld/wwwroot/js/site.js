67b4df2ccac4fbddfdf42f15f3cc798b6231c7f5
Vasilut Lucian <vasilut@outlook.com>
2017-02-18 09:49:49 +0200

    Added bower + some dependencies. Added jquery.

---
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿//js file
+(function () {
+    var ele = $("#username");
+    ele.text("Luciano");
+
+    var man = $("#main");
+
+    man.on("mouseenter",function () {
+        man.style = "background-color: #888;";
+    });
+
+    man.on("mouseleave",function () {
+        man.style = "";
+    });
+
+    var menuItems = $("ul.menu li a");
+    menuItems.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+    });
+
+})();
