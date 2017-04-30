0a73a23913a5431d1df4e521b72290a027270c92
Vasilut Lucian <vasilut@outlook.com>
2017-04-30 19:47:27 +0300

    Get the objects calling the API from angular controller.

--- TheWorld/src/TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/src/TheWorld/wwwroot/js/tripsController.js
@@ -6,18 +6,26 @@ function () {
     angular.module("app-trips")
            .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
         var vm = this;
-        vm.trips = [{
-            name: "Lucian trips",
-            created: new Date()
-        }, {
-            name: "Lucian trips 2",
-            created: new Date()
-        }
-        ];
+        vm.trips = [];
 
         vm.newTrip = {};
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                //succes
+                angular.copy(response.data, vm.trips);
+            },
+            function (error) {
+                //failure
+                vm.errorMessage = "Failed to load data: " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
 
         vm.addTrip = function () {
             vm.trips.push({ name: vm.newTrip.name, created: new Date() });
