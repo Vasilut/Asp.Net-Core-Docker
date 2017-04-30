caf68c0278171ce972278a969f34835dce3e9660
Vasilut Lucian <vasilut@outlook.com>
2017-04-30 22:51:34 +0300

    Added directive for spinner.

--- TheWorld/src/TheWorld/wwwroot/js/app-trips.js
+++ TheWorld/src/TheWorld/wwwroot/js/app-trips.js
@@ -5,6 +5,6 @@ function () {
     "use strict";
 
     //creating the module
-    angular.module("app-trips", []);
+    angular.module("app-trips", ["simpleControls"]);
 }
 )();
---
+++ TheWorld/src/TheWorld/wwwroot/js/simpleControls.js
@@ -0,0 +1,17 @@
+﻿//simpleControls.js
+
+(
+function () {
+    "use strict";
+
+    angular.module("simpleControls", [])
+        .directive("waitCursor", waitCursor);
+
+    function waitCursor() {
+        return {
+            templateUrl: "/views/waitCursor.html"
+        }
+    }
+
+}
+)();
