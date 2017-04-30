29ccc7fa3dc900c8a38be8f977fe81ab7ec619d4
Vasilut Lucian <vasilut@outlook.com>
2017-04-30 19:13:45 +0300

    Added new form + validation.

--- TheWorld/src/TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/src/TheWorld/wwwroot/js/tripsController.js
@@ -6,17 +6,23 @@ function () {
     angular.module("app-trips")
            .controller("tripsController", tripsController);
 
-    function tripsController()
-    {
+    function tripsController() {
         var vm = this;
         vm.trips = [{
-            name : "Lucian trips",
+            name: "Lucian trips",
             created: new Date()
         }, {
             name: "Lucian trips 2",
             created: new Date()
         }
         ];
+
+        vm.newTrip = {};
+
+        vm.addTrip = function () {
+            vm.trips.push({ name: vm.newTrip.name, created: new Date() });
+            vm.newTrip = {};
+        }
     }
 }
 )();
